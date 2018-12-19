a19d126aeb1688c9d7ccc85d6e19bc793c5cad32
Cristina Cristobal <cristina.cristobal@gmail.com>
2018-12-19 23:23:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -10,7 +10,7 @@ if phrase != phrase.upcase
 elsif phrase == phrase.upcase
   return "NO, NOT SINCE 1938!"
 # NO, NOT SINCE 1938!
-elsif phrase == "I LOVE YOU GRANDMA"
+elsif phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
