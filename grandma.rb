0e3019951a1dd4ed2535467cfd72a296db62b400
Cristina Cristobal <cristina.cristobal@gmail.com>
2018-12-19 23:27:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@ if phrase != phrase.upcase
   return "HUH?! SPEAK UP, SONNY!"
 # If you shout, she can hear you (or at least she thinks so)
 # and yells back
-elsif phrase == phrase.upcase
+elsif phrase = phrase.upcase
   return "NO, NOT SINCE 1938!"
 # NO, NOT SINCE 1938!
 elsif phrase == "I LOVE YOU GRANDMA!"
@@ -15,4 +15,4 @@ elsif phrase == "I LOVE YOU GRANDMA!"
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 end
-end 
+end
